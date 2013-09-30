06b54d1eec302af9bf80dfb59e38d715adc9a44b
rjcanto <rjcanto@gmail.com>
2013-09-30 23:01:08 +0100

    MyCryptoTool

--- Serie1/src/Exerc6/MyCryptoTool.java
+++ Serie1/src/Exerc6/MyCryptoTool.java
@@ -2,25 +2,58 @@ package Exerc6;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MyCryptoTool {
-	MessageDigest md ;
-	DigestInputStream dis;
-	InputStream is;
+	byte[] hashGood ;
+	byte[] hashBad ;
 	
-	public MyCryptoTool() throws NoSuchAlgorithmException, FileNotFoundException{
-		md =  MessageDigest.getInstance("SHA1");
-		is = new FileInputStream("GoodApp.java");
-		dis = new DigestInputStream(is, md);
-		byte[] hash = dis.getMessageDigest().digest();
+	public MyCryptoTool() throws NoSuchAlgorithmException, IOException{
+		hashGood = produceHash("c:\\GoodApp.java");
+		hashBad = produceHash("c:\\BadApp.java");
 		
-		for(byte b: hash)
-			System.out.println(String.format("%x", b));
+	}
+	
+	private byte[] produceHash(String filePath) throws NoSuchAlgorithmException, IOException {
+		MessageDigest md =  MessageDigest.getInstance("SHA1");
+		InputStream is = new FileInputStream(filePath);
 		
-		System.out.println("---");
+		
+		
+		byte[] data = new byte[4096];
+		
+		byte b; int i= 0;
+		
+		while ((b=(byte) is.read())!=-1 && i < data.length) {
+			data[i] = b ;
+			i++;
+		}
+		is.close();
+		return md.digest();
+	}
+	
+	private byte[] readGood(int nBytes){
+		return Arrays.copyOf(hashBad, nBytes);
+		
+	}
+	
+	private byte[] readBad(int nBytes){
+		
+		
+		return Arrays.copyOf(hashGood, nBytes);
+			
+	}
+	
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		MyCryptoTool crypto = new MyCryptoTool();
+		int nBytes = 1 ;
+		
+		System.out.println("GoodApp Hash: " + crypto.readGood(nBytes).toString()) ;
+		System.out.println("BadApp Hash: " + crypto.readBad(nBytes).toString()) ;
 	}
 }
