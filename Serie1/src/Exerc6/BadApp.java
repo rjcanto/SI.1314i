ebd7b3935a60b9c70a3dd39ad23c22fbb414edaf
rjcanto <rjcanto@gmail.com>
2013-09-30 22:06:44 +0100

    GoodApp and BadApp copy n paste

--- Serie1/src/Exerc6/BadApp.java
+++ Serie1/src/Exerc6/BadApp.java
@@ -1,5 +1,11 @@
 package Exerc6;
 
-public class BadApp {
+public class BadApp{
+
+	private static final String msg = "Hello, I'm the bad app";
+	
+	public static void main(String[] args){		
+		System.out.println(msg);
+	}
 
 }
--- Serie1/src/Exerc6/GoodApp.java
+++ Serie1/src/Exerc6/GoodApp.java
@@ -1,5 +1,11 @@
 package Exerc6;
 
-public class GoodApp {
+public class GoodApp{
+
+	private static final String msg = "Hello, I'm the good app";
+
+	public static void main(String[] args){
+		System.out.println(msg);
+	}
 
 }
