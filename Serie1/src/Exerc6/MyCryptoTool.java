5f3d5766dc790491c162114d710759672f339e9d
lbras <public@lbras.net>
2013-10-03 20:10:50 +0100

    LB Testing with full SHA1

--- Serie1/src/Exerc6/MyCryptoTool.java
+++ Serie1/src/Exerc6/MyCryptoTool.java
@@ -4,14 +4,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileSystem;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class MyCryptoTool {
-	byte[] hashGood ;
-	byte[] hashBad ;
+	private byte[] hashGood ;
+	private byte[] hashBad ;
 	
 	public MyCryptoTool() throws NoSuchAlgorithmException, IOException{
 		hashGood = produceHash("c:\\GoodApp.java");
@@ -23,8 +24,6 @@ public class MyCryptoTool {
 		MessageDigest md =  MessageDigest.getInstance("SHA1");
 		InputStream is = new FileInputStream(filePath);
 		
-		
-		
 		byte[] data = new byte[4096];
 		
 		byte b; int i= 0;
@@ -34,26 +33,32 @@ public class MyCryptoTool {
 			i++;
 		}
 		is.close();
-		return md.digest();
+		return md.digest(Arrays.copyOf(data, i));
 	}
 	
 	private byte[] readGood(int nBytes){
-		return Arrays.copyOf(hashBad, nBytes);
+		return hashGood;
+		//return Arrays.copyOf(hashGood, nBytes);
 		
 	}
 	
 	private byte[] readBad(int nBytes){
 		
-		
-		return Arrays.copyOf(hashGood, nBytes);
+		return hashBad;
+		//return Arrays.copyOf(hashBad, nBytes);
 			
 	}
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		
 		MyCryptoTool crypto = new MyCryptoTool();
 		int nBytes = 1 ;
 		
-		System.out.println("GoodApp Hash: " + crypto.readGood(nBytes).toString()) ;
+		System.out.print("GoodApp Hash: ");
+		for (byte b : crypto.readGood(nBytes))
+		{
+			System.out.printf("%x", b);
+		}
 		System.out.println("BadApp Hash: " + crypto.readBad(nBytes).toString()) ;
 	}
 }
