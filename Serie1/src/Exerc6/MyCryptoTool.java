29161201cb114e373cdb6fdbe88ee68dad9ef794
rjcanto <rjcanto@gmail.com>
2013-10-07 20:19:27 +0100

    Remove warning from import

--- Serie1/src/Exerc6/MyCryptoTool.java
+++ Serie1/src/Exerc6/MyCryptoTool.java
@@ -8,8 +8,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Random;
 
-import com.sun.xml.internal.ws.client.dispatch.MessageDispatch;
-
 public class MyCryptoTool {
 	private byte[] hashGood ;
 	private byte[] hashBad ;
