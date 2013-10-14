c39f560586ecc943765fa8800700915552e4b0a4
rjcanto <rjcanto@gmail.com>
2013-10-14 22:31:21 +0100

    The start of MyJavaIron

---
+++ Serie1/src/Exerc6/MyJavaIron.java
@@ -0,0 +1,14 @@
+package Exerc6;
+
+/**
+ * 
+ * @author Ricardo Canto
+ * @author Luís Brás
+ *
+ * A small library to use Iron in Java
+ * https://github.com/hueniverse/iron
+ */
+
+public class MyJavaIron {
+
+}
