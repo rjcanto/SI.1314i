daf9834677ef77c0d8f21703311c6663d09619f9
rjcanto <rjcanto@gmail.com>
2013-10-22 22:47:20 +0100

    cipher parameters

--- Serie1/src/Exerc6/MyJavaIron.java
+++ Serie1/src/Exerc6/MyJavaIron.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -23,7 +24,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 import com.sun.xml.internal.messaging.saaj.util.Base64;
 
 /**
@@ -62,7 +62,7 @@ public class MyJavaIron {
 		return new SecretKeySpec(key,"AES");
 	}
 	
-	public byte[] encrypt(SecretKey k, IronOptions options, String data) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException, InvalidParameterSpecException {
+	public byte[] encrypt(SecretKey k, IronOptions options, String data) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException, InvalidParameterSpecException, InvalidAlgorithmParameterException {
 		Random r = new SecureRandom();
 		byte[] hmacSalt = new byte[10] ;
 		byte[] hmac = new byte[10] ;
@@ -72,10 +72,17 @@ public class MyJavaIron {
 		
 		r.nextBytes(hmacSalt);
 		
+
+		 AlgorithmParameters algParams;
+	    algParams = AlgorithmParameters.getInstance("AES");
+
+
+		
 		Cipher cipher = Cipher.getInstance("AES");
-		cipher.init(Cipher.ENCRYPT_MODE, k);
-		AlgorithmParameters params = cipher.getParameters();
-		byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+		cipher.init(Cipher.ENCRYPT_MODE, k,algParams);
+		//AlgorithmParameters params = cipher.getParameters();
+		//byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+		byte[] iv = algParams.getParameterSpec(IvParameterSpec.class).getIV();
 		byte[] ciphertext = cipher.doFinal(data.getBytes());
 		byte[] encryptedB64 = Base64.encode(ciphertext);
 		
@@ -103,7 +110,7 @@ public class MyJavaIron {
 		return null;
 	}
 	
-	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, IOException {
+	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidAlgorithmParameterException {
 		final String password = "TESTE";
 		final String jsonObj = "{\"course\":\"LEIC\",\"is_Optional\":true,\"name\":\"SI1314i\"}" ;
 		
