1705ff1d5be93dac3773a1fb6cdcece5884fcc65
lbras <public@lbras.net>
2013-10-24 22:08:47 +0100

    LB

--- Serie1/src/Exerc6/MyJavaIron.java
+++ Serie1/src/Exerc6/MyJavaIron.java
@@ -1,12 +1,9 @@
 package Exerc6;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.AlgorithmParameters;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -20,13 +17,10 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import sun.misc.BASE64Encoder;
-
-import com.sun.xml.internal.messaging.saaj.util.Base64;
+import org.apache.commons.codec.binary.Base64;
 
 /**
  * 
@@ -45,68 +39,42 @@ public class MyJavaIron {
 	public SecretKey generate(String password, IronOptions options) throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException {
 		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHMacSHA1");
 		Random r = new SecureRandom();
-		r.nextBytes(options.salt); 
-		//byte[] key = new byte[20];
-		
+		r.nextBytes(options.salt);		
 		
 		KeySpec spec = new PBEKeySpec(password.toCharArray(),
 										options.salt,
 										options.iterations,
-										options.saltBits
+										256
 									);
 		SecretKey k = f.generateSecret(spec);
 		return new SecretKeySpec(k.getEncoded(), "AES");
-		
-		//MessageDigest digester = MessageDigest.getInstance("SHA-256");
-		//digester.update(password.getBytes("UTF-8"));
-		//byte[] key = digester.digest();
-		//System.out.println(key.length);
-		//return new SecretKeySpec(key,"AES");
 	}
 	
-	public byte[] encrypt(SecretKey k, IronOptions options, String data) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException, InvalidParameterSpecException, InvalidAlgorithmParameterException {
+	public String encrypt(SecretKey k, IronOptions options, String data) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException, InvalidParameterSpecException, InvalidAlgorithmParameterException {
 		Random r = new SecureRandom();
 		byte[] hmacSalt = new byte[10] ;
-		byte[] hmac = new byte[10] ;
-		ByteArrayOutputStream sealedObjStream = new ByteArrayOutputStream( );
+		byte[] hmac = new byte[10] ;		
 		
 		String macPrefix = "fe26.2" ;
 		
 		r.nextBytes(hmacSalt);
-		
 
-		/*AlgorithmParameters algParams;
-	    algParams = AlgorithmParameters.getInstance("AES");
-	    algParams.init(new IvParameterSpec(new byte[] {));
-		*/
-		
 		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		cipher.init(Cipher.ENCRYPT_MODE, k, new SecureRandom());
 		byte[] iv = cipher.getIV();
-		//AlgorithmParameters params = cipher.getParameters();
-		//byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
-		//byte[] iv = algParams.getParameterSpec(IvParameterSpec.class).getIV();
-		byte[] ciphertext = cipher.doFinal(data.getBytes());
-		BASE64Encoder encoder = new BASE64Encoder();
-		byte[] encryptedB64 = encoder.encode(ciphertext).getBytes();
 		
-		sealedObjStream.write(macPrefix.getBytes());
-		sealedObjStream.write("**".getBytes());
-		sealedObjStream.write(options.salt);
-		sealedObjStream.write("*".getBytes());
-		sealedObjStream.write(iv);
-		sealedObjStream.write("*".getBytes());
-		sealedObjStream.write(encryptedB64);
-		sealedObjStream.write("**".getBytes());
+		byte[] ciphertext = cipher.doFinal(data.getBytes());		
 		
-		Random hmacRandom = new SecureRandom(sealedObjStream.toByteArray( ));
+		String sealed =
+				macPrefix + "**" +
+				Base64.encodeBase64String(options.salt) + "*" +
+				Base64.encodeBase64String(iv) + "*" +		
+				Base64.encodeBase64String(ciphertext) + "**";
+		
+		Random hmacRandom = new SecureRandom(sealed.getBytes());
 		hmacRandom.nextBytes(hmac) ;
 		
-		sealedObjStream.write(hmacSalt);
-		sealedObjStream.write("*".getBytes());
-		sealedObjStream.write(hmac);
-		
-		return sealedObjStream.toByteArray( );
+		return sealed + Base64.encodeBase64String(hmacSalt) + "*" + Base64.encodeBase64String(hmac);
 	}
 	
 	
@@ -116,11 +84,12 @@ public class MyJavaIron {
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidAlgorithmParameterException {
 		final String password = "TESTE";
-		final String jsonObj = "{\"course\":\"LEIC\",\"is_Optional\":true,\"name\":\"SI1314i\"}" ;
+		//final String jsonObj = "{\"course\":\"LEIC\",\"is_Optional\":true,\"name\":\"SI1314i\"}" ;
+		final String jsonObj = "{a: 6}" ;
 		
 		MyJavaIron iron = new MyJavaIron() ;
 		IronOptions io = new IronOptions();
 		 
-		System.out.println(iron.encrypt(iron.generate(password,io), io, jsonObj).toString());
+		System.out.println(iron.encrypt(iron.generate(password,io), io, jsonObj));
 	}
 }
