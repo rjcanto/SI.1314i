05e10d818595eb8eef411d4dec60527bfd294c69
rjcanto <rjcanto@gmail.com>
2013-11-23 18:10:36 +0000

    Initial commit for Identity Provider

---
+++ Serie2_IdentityProvider/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Serie2_IdentityProvider
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Serie2_IdentityProvider/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Serie2_IdentityProvider.MvcApplication" Language="C#" %>
