744b3d26d21974974170ff0bc37b56b7ffd65f4c
lbras <public@lbras.net>
2013-12-12 20:19:54 +0000

    LB Build script creates DB. Some tables created.

--- Serie3/PDPDB/buildTables.sql
+++ Serie3/PDPDB/buildTables.sql
@@ -1,4 +1,29 @@
--- DROP
+if OBJECT_ID('PDPDB') is null
+	CREATE DATABASE PDPDB;
+GO
 
+USE PDPDB;
+
+-- DROP
+if OBJECT_ID('User') is not null
+	drop table [User];
+if OBJECT_ID('Role') is not null
+	drop table [Role];
+GO
 
 -- CREATE
+CREATE TABLE [User]
+	(
+	userId int IDENTITY,		
+	username varchar(64) UNIQUE,
+	constraint PK_userId primary key (userId)
+	);
+GO
+
+CREATE TABLE [Role]
+	(
+	roleId int IDENTITY,		
+	rolename varchar(64) UNIQUE,
+	constraint PK_roleId primary key (roleId)
+	);
+GO
