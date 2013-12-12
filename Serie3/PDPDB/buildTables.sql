e93b15f8ce741454c2252e0a3cdca86d9051b5fc
lbras <public@lbras.net>
2013-12-12 21:03:49 +0000

    LB Tables added

--- Serie3/PDPDB/buildTables.sql
+++ Serie3/PDPDB/buildTables.sql
@@ -1,14 +1,25 @@
-if OBJECT_ID('PDPDB') is null
+DECLARE @dbname nvarchar(128);
+SET @dbname = 'PDPDB';
+
+IF (NOT EXISTS (SELECT name 
+			FROM master.dbo.sysdatabases 
+			WHERE ('[' + name + ']' = @dbname OR name = @dbname)))
 	CREATE DATABASE PDPDB;
 GO
 
 USE PDPDB;
 
 -- DROP
-if OBJECT_ID('User') is not null
-	drop table [User];
+if OBJECT_ID('Action') is not null
+	drop table [Action];
+if OBJECT_ID('Permission') is not null
+	drop table [Permission];
+if OBJECT_ID('Resource') is not null
+	drop table [Resouce];
 if OBJECT_ID('Role') is not null
 	drop table [Role];
+if OBJECT_ID('User') is not null
+	drop table [User];
 GO
 
 -- CREATE
@@ -25,5 +36,29 @@ CREATE TABLE [Role]
 	roleId int IDENTITY,		
 	rolename varchar(64) UNIQUE,
 	constraint PK_roleId primary key (roleId)
+	);
+GO
+
+CREATE TABLE [Resource]
+	(
+	resourceId int IDENTITY,		
+	resourceName varchar(64) UNIQUE,
+	constraint PK_resourceId primary key (resourceId)
+	);
+GO
+
+CREATE TABLE [Permission]
+	(
+	permissionId int IDENTITY,		
+	permissionName varchar(64) UNIQUE,
+	constraint PK_permissionId primary key (permissionId)
+	);
+GO
+
+CREATE TABLE [Action]
+	(
+	actionId int IDENTITY,		
+	actionName varchar(64) UNIQUE,
+	constraint PK_actionId primary key (actionId)
 	);
 GO
