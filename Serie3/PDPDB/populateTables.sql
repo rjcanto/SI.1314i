5393596ea29216f316fd7f29823e0b29e161325a
lbras <public@lbras.net>
2013-12-12 21:36:24 +0000

    LB Populate user

---
+++ Serie3/PDPDB/populateTables.sql
@@ -0,0 +1,13 @@
+-- Populate USER
+INSERT INTO [User](userId, username) values ('Ricardo');
+INSERT INTO [User](userId, username) values ('Luís');
+INSERT INTO [User](userId, username) values ('Pedro');
+INSERT INTO [User](userId, username) values ('João');
+INSERT INTO [User](userId, username) values ('Teresa');
+INSERT INTO [User](userId, username) values ('Lídia');
+INSERT INTO [User](userId, username) values ('Sara');
+INSERT INTO [User](userId, username) values ('Elsa');
+INSERT INTO [User](userId, username) values ('Mário');
+INSERT INTO [User](userId, username) values ('Cristina');
+INSERT INTO [User](userId, username) values ('Maria');
+INSERT INTO [User](userId, username) values ('Miguel');
