dd66182cbc32840a24aec150afb223008d15d20b
lbras <public@lbras.net>
2013-12-17 19:11:51 +0000

    LB Finished DB schema and test data

--- Serie3/PDPDB/buildTables.sql
+++ Serie3/PDPDB/buildTables.sql
@@ -9,15 +9,23 @@ GO
 
 USE PDPDB;
 
--- DROP
-if OBJECT_ID('RolePermissions') is not null
-	drop table [RolePermissions];
+-- Deltas
 if OBJECT_ID('UserRoles') is not null
 	drop table [UserRoles];
-if OBJECT_ID('Action') is not null
-	drop table [Action];
+if OBJECT_ID('RolePermissions') is not null
+	drop table [RolePermissions];
+
+-- DROP
+if OBJECT_ID('PermissionAssignment') is not null
+	drop table [PermissionAssignment];
 if OBJECT_ID('Permission') is not null
 	drop table [Permission];
+if OBJECT_ID('RoleHierarchy') is not null
+	drop table [RoleHierarchy];
+if OBJECT_ID('UserAssignment') is not null
+	drop table [UserAssignment];
+if OBJECT_ID('Action') is not null
+	drop table [Action];
 if OBJECT_ID('Resource') is not null
 	drop table [Resource];
 if OBJECT_ID('Role') is not null
@@ -39,9 +47,7 @@ CREATE TABLE [Role]
 	(
 	roleId int IDENTITY,		
 	rolename varchar(64) UNIQUE,
-	parentRoleId int,
 	constraint PK_roleId primary key (roleId),
-	constraint FK_Role_parentRoleId foreign key(parentRoleId) references [Role](roleId)
 	);
 GO
 
@@ -53,14 +59,6 @@ CREATE TABLE [Resource]
 	);
 GO
 
-CREATE TABLE [Permission]
-	(
-	permissionId int IDENTITY,		
-	permissionName varchar(64) UNIQUE,
-	constraint PK_permissionId primary key (permissionId)
-	);
-GO
-
 CREATE TABLE [Action]
 	(
 	actionId int IDENTITY,		
@@ -69,22 +67,43 @@ CREATE TABLE [Action]
 	);
 GO
 
-CREATE TABLE UserRoles
+CREATE TABLE UserAssignment
 	(
 	userId int,		
 	roleId int,
-	constraint PK_UserRoles primary key (userId, roleId),
-	constraint FK_UserRoles_userId foreign key(userId) references [User](userId),
-	constraint FK_UserRoles_roleId foreign key(roleId) references [Role](roleId)
+	constraint PK_UserAssignment primary key (userId, roleId),
+	constraint FK_UserAssignment_userId foreign key(userId) references [User](userId),
+	constraint FK_UserAssignment_roleId foreign key(roleId) references [Role](roleId)
 	);
 GO
 
-CREATE TABLE RolePermissions
+CREATE TABLE RoleHierarchy
+	(
+	roleId int,		
+	juniorRoleId int,
+	constraint PK_JuniorRoles primary key (roleId, juniorRoleId),
+	constraint FK_JuniorRoles_userId foreign key(roleId) references [Role](roleId),
+	constraint FK_JuniorRoles_juniorRoleId foreign key(roleId) references [Role](roleId)
+	);
+GO
+
+CREATE TABLE Permission
+	(
+	resourceId int,
+	actionId int,
+	constraint PK_PermissionResource primary key (actionId, resourceId),
+	constraint FK_PermissionResource_actionId foreign key(actionId) references [Action](actionId),
+	constraint FK_PermissionResource_resourceId foreign key(resourceId) references [Resource](resourceId)
+	);
+GO
+
+CREATE TABLE PermissionAssignment
 	(
 	roleId int,
-	permissionId int,
-	constraint PK_RolePermissions primary key (roleId, permissionId),	
-	constraint FK_RolePermissions_roleId foreign key(roleId) references [Role](roleId),
-	constraint FK_RolePermissions_userId foreign key(permissionId) references [Permission](permissionId)
+	actionId int,
+	resourceId int,
+	constraint PK_PermissionAssignment primary key (roleId, actionId, resourceId),	
+	constraint FK_PermissionAssignment_roleId foreign key(roleId) references [Role](roleId),
+	constraint FK_PermissionAssignment_actionId_resourceId foreign key(actionId, resourceId) references [Permission](actionId, resourceId)
 	);
 GO
--- Serie3/PDPDB/populateTables.sql
+++ Serie3/PDPDB/populateTables.sql
@@ -1,45 +1,116 @@
 USE PDPDB
 
 -- Populate USER
-INSERT INTO [User](username) values ('Ricardo');
-INSERT INTO [User](username) values ('Luís');
-INSERT INTO [User](username) values ('Pedro');
-INSERT INTO [User](username) values ('João');
-INSERT INTO [User](username) values ('Teresa');
-INSERT INTO [User](username) values ('Lídia');
-INSERT INTO [User](username) values ('Sara');
-INSERT INTO [User](username) values ('Elsa');
-INSERT INTO [User](username) values ('Mário');
-INSERT INTO [User](username) values ('Cristina');
-INSERT INTO [User](username) values ('Maria');
-INSERT INTO [User](username) values ('Miguel');
+INSERT INTO [User](username) VALUES ('Ricardo');
+INSERT INTO [User](username) VALUES ('Luís');
+INSERT INTO [User](username) VALUES ('Pedro');
+INSERT INTO [User](username) VALUES ('João');
+INSERT INTO [User](username) VALUES ('Teresa');
+INSERT INTO [User](username) VALUES ('Lídia');
+INSERT INTO [User](username) VALUES ('Sara');
+INSERT INTO [User](username) VALUES ('Elsa');
+INSERT INTO [User](username) VALUES ('Mário');
+INSERT INTO [User](username) VALUES ('Cristina');
+INSERT INTO [User](username) VALUES ('Maria');
+INSERT INTO [User](username) VALUES ('Miguel');
 
--- Populate ROLE
-INSERT INTO [Role](roleName) values ('Admin');
-INSERT INTO [Role](roleName) values ('Manager');
-INSERT INTO [Role](roleName) values ('User');
-INSERT INTO [Role](roleName) values ('Guest');
+-- Populate Role
+INSERT INTO [Role](roleName) VALUES ('Admin');
+INSERT INTO [Role](roleName) VALUES ('Director');
+INSERT INTO [Role](roleName) VALUES ('Manager');
+INSERT INTO [Role](roleName) VALUES ('Auditor');
+INSERT INTO [Role](roleName) VALUES ('User');
+INSERT INTO [Role](roleName) VALUES ('Guest');
 
--- Populate UserRoles
+-- Populate RoleHierarchy
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (1, 2);
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (1, 3);
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (2, 5);
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (3, 5);
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (4, 5);
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (5, 6);
+
+-- Populate UserAssignment
 --Admin
-INSERT INTO [UserRoles](userId, roleId) values (1, 1);
-INSERT INTO [UserRoles](userId, roleId) values (2, 1);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (1, 1);
+--Director
+INSERT INTO [UserAssignment](userId, roleId) VALUES (2, 2);
 --Manager
-INSERT INTO [UserRoles](userId, roleId) values (3, 2);
-INSERT INTO [UserRoles](userId, roleId) values (4, 2);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (3, 3);
+--Auditor
+INSERT INTO [UserAssignment](userId, roleId) VALUES (4, 4);
 --User
-INSERT INTO [UserRoles](userId, roleId) values (5, 3);
-INSERT INTO [UserRoles](userId, roleId) values (6, 3);
-INSERT INTO [UserRoles](userId, roleId) values (7, 3);
-INSERT INTO [UserRoles](userId, roleId) values (8, 3);
-INSERT INTO [UserRoles](userId, roleId) values (9, 3);
-INSERT INTO [UserRoles](userId, roleId) values (10, 3);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (5, 5);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (6, 5);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (7, 5);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (8, 5);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (9, 5);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (10, 5);
 --Guest
-INSERT INTO [UserRoles](userId, roleId) values (11, 4);
-INSERT INTO [UserRoles](userId, roleId) values (12, 4)
+INSERT INTO [UserAssignment](userId, roleId) VALUES (11, 6);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (12, 6)
 
---Populate Permission
---INSERT INTO [Permission](permissionName) values ('');
+-- Populate Action
+INSERT INTO [Action](actionName) VALUES ('Criar ficheiros e pastas');
+INSERT INTO [Action](actionName) VALUES ('Ver o conteúdo de ficheiros e listar conteúdo de pastas');
+INSERT INTO [Action](actionName) VALUES ('Alterar ficheiros');
+INSERT INTO [Action](actionName) VALUES ('Eliminar ficheiros e pastas');
+INSERT INTO [Action](actionName) VALUES ('Executar ficheiros');
 
---Populate RolePermissions
---INSERT INTO [RolePermissions](roleId, permissionId) values (11, 4);
+-- Populate Resources
+INSERT INTO [Resource](resourceName) VALUES ('/folder');
+INSERT INTO [Resource](resourceName) VALUES ('/folder/file1.txt');
+INSERT INTO [Resource](resourceName) VALUES ('/folder/file2.txt');
+INSERT INTO [Resource](resourceName) VALUES ('/folder/file3.txt');
+INSERT INTO [Resource](resourceName) VALUES ('/program.exe');
+
+-- Populate Permission
+INSERT INTO [Permission](resourceId, actionId) VALUES (1, 1);
+INSERT INTO [Permission](resourceId, actionId) VALUES (1, 2);
+INSERT INTO [Permission](resourceId, actionId) VALUES (1, 3);
+INSERT INTO [Permission](resourceId, actionId) VALUES (1, 4);
+INSERT INTO [Permission](resourceId, actionId) VALUES (1, 5);
+INSERT INTO [Permission](resourceId, actionId) VALUES (2, 2);
+INSERT INTO [Permission](resourceId, actionId) VALUES (2, 3);
+INSERT INTO [Permission](resourceId, actionId) VALUES (2, 4);
+INSERT INTO [Permission](resourceId, actionId) VALUES (3, 2);
+INSERT INTO [Permission](resourceId, actionId) VALUES (3, 3);
+INSERT INTO [Permission](resourceId, actionId) VALUES (3, 4);
+INSERT INTO [Permission](resourceId, actionId) VALUES (4, 2);
+INSERT INTO [Permission](resourceId, actionId) VALUES (5, 5);
+
+-- Populate PermissionAssignment
+--Admin
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 1, 1);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 1, 2);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 1, 3);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 1, 4);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 1, 5);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 2, 2);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 2, 3);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 2, 4);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 3, 2);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 3, 3);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 3, 4);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 4, 2);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 5, 5);
+-- Director
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (2, 1, 1);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (2, 1, 2);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (2, 1, 3);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (2, 1, 4);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (2, 1, 5);
+-- Manager
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (3, 1, 1);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (3, 1, 3);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (3, 1, 4);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (3, 1, 5);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (3, 3, 2);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (3, 4, 2);
+-- Auditor
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (4, 1, 2);
+-- User
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (5, 1, 1);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (5, 1, 5);
+-- Guest
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (6, 5, 5);
