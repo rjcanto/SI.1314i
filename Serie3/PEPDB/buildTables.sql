61e6327d5a616dc4209942203fc93b4601a1f912
lbras <public@lbras.net>
2014-01-09 22:35:54 +0000

    LB PEP database

---
+++ Serie3/PEPDB/buildTables.sql
@@ -0,0 +1,113 @@
+DECLARE @dbname nvarchar(128);
+SET @dbname = 'PEPDB';
+
+IF (NOT EXISTS (SELECT name 
+			FROM master.dbo.sysdatabases 
+			WHERE ('[' + name + ']' = @dbname OR name = @dbname)))
+	CREATE DATABASE PEPDB;
+GO
+
+USE PEPDB;
+
+-- WebMatrix / Created by WebSecurity.InitializeDatabaseConnection()
+if OBJECT_ID('webpages_UsersInRoles') is not null
+	drop table [webpages_UsersInRoles];
+if OBJECT_ID('webpages_Roles') is not null
+	drop table [webpages_Roles];
+if OBJECT_ID('webpages_OAuthMembership') is not null
+	drop table [webpages_OAuthMembership];
+if OBJECT_ID('webpages_Membership') is not null
+	drop table [webpages_Membership];
+
+-- DROP
+if OBJECT_ID('PermissionAssignment') is not null
+	drop table [PermissionAssignment];
+if OBJECT_ID('Permission') is not null
+	drop table [Permission];
+if OBJECT_ID('RoleHierarchy') is not null
+	drop table [RoleHierarchy];
+if OBJECT_ID('UserAssignment') is not null
+	drop table [UserAssignment];
+if OBJECT_ID('Action') is not null
+	drop table [Action];
+if OBJECT_ID('Resource') is not null
+	drop table [Resource];
+if OBJECT_ID('Role') is not null
+	drop table [Role];
+if OBJECT_ID('User') is not null
+	drop table [User];
+GO
+
+-- CREATE
+CREATE TABLE [User]
+	(
+	userId int IDENTITY,		
+	username varchar(64) UNIQUE,
+	constraint PK_userId primary key (userId)
+	);
+GO
+
+CREATE TABLE [Role]
+	(
+	roleId int IDENTITY,		
+	rolename varchar(64) UNIQUE,
+	constraint PK_roleId primary key (roleId),
+	);
+GO
+
+CREATE TABLE [Resource]
+	(
+	resourceId int IDENTITY,		
+	resourceName varchar(64) UNIQUE,
+	constraint PK_resourceId primary key (resourceId)
+	);
+GO
+
+CREATE TABLE [Action]
+	(
+	actionId int IDENTITY,		
+	actionName varchar(64) UNIQUE,
+	constraint PK_actionId primary key (actionId)
+	);
+GO
+
+CREATE TABLE UserAssignment
+	(
+	userId int,		
+	roleId int,
+	constraint PK_UserAssignment primary key (userId, roleId),
+	constraint FK_UserAssignment_userId foreign key(userId) references [User](userId),
+	constraint FK_UserAssignment_roleId foreign key(roleId) references [Role](roleId)
+	);
+GO
+
+CREATE TABLE RoleHierarchy
+	(
+	roleId int,		
+	juniorRoleId int,
+	constraint PK_JuniorRoles primary key (roleId, juniorRoleId),
+	constraint FK_JuniorRoles_userId foreign key(roleId) references [Role](roleId),
+	constraint FK_JuniorRoles_juniorRoleId foreign key(roleId) references [Role](roleId)
+	);
+GO
+
+CREATE TABLE Permission
+	(
+	resourceId int,
+	actionId int,
+	constraint PK_PermissionResource primary key (actionId, resourceId),
+	constraint FK_PermissionResource_actionId foreign key(actionId) references [Action](actionId),
+	constraint FK_PermissionResource_resourceId foreign key(resourceId) references [Resource](resourceId)
+	);
+GO
+
+CREATE TABLE PermissionAssignment
+	(
+	roleId int,
+	actionId int,
+	resourceId int,
+	constraint PK_PermissionAssignment primary key (roleId, actionId, resourceId),	
+	constraint FK_PermissionAssignment_roleId foreign key(roleId) references [Role](roleId),
+	constraint FK_PermissionAssignment_actionId_resourceId foreign key(actionId, resourceId) references [Permission](actionId, resourceId)
+	);
+GO
---
+++ Serie3/PEPDB/populateTables.sql
@@ -0,0 +1,51 @@
+USE PEPDB
+
+-- Populate USER
+INSERT INTO [User](username) VALUES ('ricardo');
+INSERT INTO [User](username) VALUES ('luis');
+INSERT INTO [User](username) VALUES ('user');
+INSERT INTO [User](username) VALUES ('guest');
+
+-- Populate Role
+INSERT INTO [Role](roleName) VALUES ('admin');
+INSERT INTO [Role](roleName) VALUES ('user');
+INSERT INTO [Role](roleName) VALUES ('guest');
+
+-- Populate RoleHierarchy
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (1, 2);
+INSERT INTO [RoleHierarchy](roleId, juniorRoleId) VALUES (2, 3);
+
+-- Populate UserAssignment
+--Admin
+INSERT INTO [UserAssignment](userId, roleId) VALUES (1, 1);
+INSERT INTO [UserAssignment](userId, roleId) VALUES (2, 1);
+--user
+INSERT INTO [UserAssignment](userId, roleId) VALUES (3, 2);
+--guest
+INSERT INTO [UserAssignment](userId, roleId) VALUES (4, 3);
+
+-- Populate Action
+INSERT INTO [Action](actionName) VALUES ('get');
+INSERT INTO [Action](actionName) VALUES ('post');
+INSERT INTO [Action](actionName) VALUES ('put');
+INSERT INTO [Action](actionName) VALUES ('head');
+INSERT INTO [Action](actionName) VALUES ('delete');
+
+-- Populate Resources
+INSERT INTO [Resource](resourceName) VALUES ('/protected/u/ricardo');
+INSERT INTO [Resource](resourceName) VALUES ('/protected/u/luis');
+INSERT INTO [Resource](resourceName) VALUES ('/protected/u/user');
+INSERT INTO [Resource](resourceName) VALUES ('/protected/u/guest');
+
+-- Populate Permission
+INSERT INTO [Permission](resourceId, actionId) VALUES (1, 1);
+INSERT INTO [Permission](resourceId, actionId) VALUES (2, 1);
+INSERT INTO [Permission](resourceId, actionId) VALUES (3, 1);
+INSERT INTO [Permission](resourceId, actionId) VALUES (4, 1);
+
+-- Populate PermissionAssignment
+--Admin
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 1, 1);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (1, 2, 1);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (2, 3, 1);
+INSERT INTO [PermissionAssignment](roleId, resourceId, actionId) VALUES (3, 4, 1);
